d95acdc2d42d7b399ddc681a81068c10a6f89aea
Alan Oberto <alanoberto@gmail.com>
2020-04-18 21:58:30 -0300

    v1 crear db

---
+++ scripts/scrip-crear-db.sql
@@ -0,0 +1,1 @@
+CREATE TABLE pelicula (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,titulo VARCHAR(100),duracion INT(5),director VARCHAR(400),anio INT(5),fecha_lanzamiento DATE,puntuacion INT(2),poster VARCHAR(300),trama VARCHAR(700))
