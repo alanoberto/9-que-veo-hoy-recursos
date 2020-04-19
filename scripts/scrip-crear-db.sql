5ecd04603bb6e5716e405816019d2b3e3815a2da
Alan Oberto <alanoberto@gmail.com>
2020-04-18 22:04:06 -0300

    agrego comando para correr script mysql

--- scripts/scrip-crear-db.sql
+++ scripts/scrip-crear-db.sql
@@ -1,1 +1,3 @@
 CREATE TABLE pelicula (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,titulo VARCHAR(100),duracion INT(5),director VARCHAR(400),anio INT(5),fecha_lanzamiento DATE,puntuacion INT(2),poster VARCHAR(300),trama VARCHAR(700))
+
+source C:\Users\Usuario1\Google Drive\Cursos\Full Stack\9-que-veo-hoy-recursos\scripts\script-paso-1-peliculas.sql
