93355287e41888a4643f873d5a33f7ae6cf03d88
Alan Oberto <alanoberto@gmail.com>
2020-05-02 11:19:24 -0300

    cambio nombre de DB por peliculas

--- servidor/lib/conexionbd.js
+++ servidor/lib/conexionbd.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     port: "3306",
     user: "root",
     password: "Uvvdu2020!",
-    database: "pelicula"
+    database: "peliculas"
 });
 
 module.exports = connection;
