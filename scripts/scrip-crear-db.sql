e62ba432f622ec87693fe607ac12ca1ee02a1f19
Alan Oberto <alanoberto@gmail.com>
2020-05-03 19:41:19 -0300

    remuevo rutas absolutas

--- scripts/scrip-crear-db.sql
+++ scripts/scrip-crear-db.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE peliculas;
 USE peliculas;
 CREATE TABLE pelicula (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,titulo VARCHAR(100),duracion INT(5),director VARCHAR(400),anio INT(5),fecha_lanzamiento DATE,puntuacion INT(2),poster VARCHAR(300),trama VARCHAR(700));
-SOURCE C:/Users/Usuario1/Google Drive/Cursos/Full Stack/9-que-veo-hoy-recursos/scripts/script-paso-1-peliculas.sql;
+SOURCE script-paso-1-peliculas.sql;
 CREATE TABLE genero (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,nombre VARCHAR(30));
 alter table pelicula add column genero_id INT(5) after trama;
-SOURCE C:/Users/Usuario1/Google Drive/Cursos/Full Stack/9-que-veo-hoy-recursos/scripts/script-paso-2-generos.sql;
+SOURCE script-paso-2-generos.sql;
 CREATE TABLE actor (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,nombre VARCHAR(30));
 CREATE TABLE actor_pelicula (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,actor_id INT(6),pelicula_id INT(6));
-source C:/Users/Usuario1/Google Drive/Cursos/Full Stack/9-que-veo-hoy-recursos/scripts/script-paso-3-actores.sql
+source script-paso-3-actores.sql
